70e4de85befdb2e029fda5bd3711c8bb34ae38cd
NaelHub <nael.hub@gmail.com>
2021-12-02 04:20:39 +0000

    play

---
+++ w2/d3/play.js
@@ -0,0 +1,9 @@
+// establishes a connection with the game server
+const {connect} = require("./client");
+ 
+const { setupInput } = require("./input");
+
+console.log("Connecting ...");
+const conn = connect();
+
+setupInput(conn)
