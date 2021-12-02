8c30698f28eadcd17f941bc3f15d9cf24b7cd789
NaelHub <nael.hub@gmail.com>
2021-12-02 04:20:13 +0000

    input

---
+++ w2/d3/input.js
@@ -0,0 +1,39 @@
+// const {connect} = require("./client");
+
+let conn = 0;
+const setupInput = (connection) => {
+  conn = connection
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    conn.write("Move: up");
+  }
+  if (key === 'a') {
+    conn.write("Move: left");
+  }
+  if (key === 's') {
+    conn.write("Move: down");
+  }
+  if (key === 'd') {
+    conn.write("Move: right");
+  }
+  // your code here
+  
+};
+
+
+
+
+
+// setupInput();
+module.exports = {setupInput, handleUserInput}  
